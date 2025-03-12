182ebe21b3d97c756ef13dae7a332c8d96cffd71
Walter Silvestre Coan <walter.coan@gmail.com>
2025-03-12 14:47:28 -0300

    intro

---
+++ copilot-dia1/src/main/java/br/com/kasolution/exercicio1/Main.java
@@ -0,0 +1,9 @@
+package br.com.kasolution.exercicio1;
+
+public class Main {
+    // defina um método main para esta classe
+    public static void main(String[] args) {
+        
+    }        
+
+}
