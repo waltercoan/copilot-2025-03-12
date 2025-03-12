bbcd45d01cc4af57e63a69481fcf21beece63252
Walter Silvestre Coan <walter.coan@gmail.com>
2025-03-12 14:56:36 -0300

    exerc1

--- copilot-dia1/src/main/java/br/com/kasolution/exercicio1/Main.java
+++ copilot-dia1/src/main/java/br/com/kasolution/exercicio1/Main.java
@@ -3,7 +3,20 @@ package br.com.kasolution.exercicio1;
 public class Main {
     // defina um método main para esta classe
     public static void main(String[] args) {
-        
+        // defina o código para chamar o método calcularMedia
+        double[] numeros = {10.0, 20.0, 30.0, 40.0, 50.0};
+        double media = calcularMedia(numeros);
+        // defina o código para imprimir a média
+        System.out.println("A média é: " + media);
     }        
+
+    // defina um método para calcular a média de uma lista de número
+    public static double calcularMedia(double[] numeros) {
+        double soma = 0;
+        for (double numero : numeros) {
+            soma += numero;
+        }
+        return soma / numeros.length;
+    }
 
 }
